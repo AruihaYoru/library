0331816e930d6868f658a19871093c08d14739c8
AruihaYoru <AruihaYoru@gmail.com>
2026-04-29 16:26:25 +0900

    なんでbuild成果物をignoreする設定なんだ

---
+++ core/pac_core_bg.wasm.d.ts
@@ -0,0 +1,17 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const __wbg_get_pacresult_success: (a: number) => number;
+export const __wbg_pacresult_free: (a: number, b: number) => void;
+export const __wbg_set_pacresult_success: (a: number, b: number) => void;
+export const decrypt: (a: number, b: number, c: number, d: number) => number;
+export const encrypt: (a: number, b: number, c: number, d: number, e: number) => [number, number, number, number];
+export const pacresult_data: (a: number) => [number, number];
+export const __wbindgen_exn_store: (a: number) => void;
+export const __externref_table_alloc: () => number;
+export const __wbindgen_externrefs: WebAssembly.Table;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __externref_table_dealloc: (a: number) => void;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_start: () => void;
